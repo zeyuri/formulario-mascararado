b0af2f2a59b565fb606543156e6a654d826fac45
Zé Yuri <jozeyuri@gmail.com>
2020-08-30 13:22:06 -0300

    remove boilerplate do CRA e adiciona form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useForm, Controller } from "react-hook-form"
+import NumberFormat from "react-number-format"
+import "./App.css"
 
 function App() {
+  const { handleSubmit, register, control } = useForm({ defaultValues })
+
+  const onSubmit = (data) => console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <form onSubmit={handleSubmit(onSubmit)} className="form">
+      <header>
+        <h1>Cadastre-se</h1>
       </header>
-    </div>
-  );
+      <div>
+        <section>
+          <label>Nome</label>
+          <input
+            name="nome"
+            className="input"
+            ref={register}
+            placeholder="Seu Nome Completo"
+          />
+        </section>
+        <section>
+          <label>Cpf</label>
+          <Controller
+            as={NumberFormat}
+            label="CPF"
+            name="cpf"
+            placeholder="000.000.000-00"
+            className="input"
+            control={control}
+            format="###.###.###-##"
+          />
+        </section>
+        <button type="submit" className="button">
+          cadastrar
+        </button>
+      </div>
+    </form>
+  )
 }
 
-export default App;
+export default App
+
+const defaultValues = {
+  nome: "",
+  cpf: "",
+}
